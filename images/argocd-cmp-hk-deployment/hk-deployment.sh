26485bb0badbf15e52ea771e10831b54aa164665
Frederic Leger <frederic@webofmars.com>
2025-11-21 09:52:42 +0100

    feat: add hk-deployment argocd CMP
    Signed-off-by: Emilien Escalle <emilien.escalle@escemi.com>

---
+++ images/argocd-cmp-hk-deployment/hk-deployment.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# save stdout descriptor
+exec 3>&1
+# redirect all output to stderr
+exec 1>&2
+
+SOURCE_DIR="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
+
+echo "hk-deployment"
+echo "version 0.1.0"
+
+echo "+ init"
+cp "$SOURCE_DIR/kustomize-template.yaml" ./kustomization.yaml
+if [ -z "${ARGOCD_ENV_HK_DEPLOYMENT_ID}" ]; then
+	ARGOCD_ENV_HK_DEPLOYMENT_ID="unknown"
+fi
+sed -i'' -e "s/<HK_DEPLOYMENT_ID>/${ARGOCD_ENV_HK_DEPLOYMENT_ID}/" kustomization.yaml
+
+echo "+ running helm"
+/usr/local/bin/helm template . \
+	--name-template ${ARGOCD_APP_NAME:-unknown} \
+	--namespace ${ARGOCD_APP_NAMESPACE:-default} \
+	--kube-version ${KUBE_VERSION:-1.31.0} >all.yaml
+
+echo "+ running kustomize"
+# this is the only command that should write to the saved stdout
+/usr/local/bin/kustomize build >&3
+
+echo "+ finished"
+exit 0
