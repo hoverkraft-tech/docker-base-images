3b6e8f5a5ed4913cf0145bf974673555514f4316
Frederic Leger <frederic@webofmars.com>
2026-06-24 22:50:28 +0200

    feat: add more tools and misc fixes

--- images/codespace-like/src/scripts/user/03-ssh.sh
+++ images/codespace-like/src/scripts/user/03-ssh.sh
@@ -5,7 +5,7 @@ set -eu -o pipefail
 export DEBIAN_FRONTEND=noninteractive
 
 if [ ! -f "/home/coder/work/.ssh/id_ed25519" ]; then
-	mise run generate-ssh-key
+	mise run ssh:setup
 else
 	echo "ssh-key exist skipping..."
 fi
